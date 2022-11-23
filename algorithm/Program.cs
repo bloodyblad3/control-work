a54a777bd9a9a82f2ee6ea5571ed463ed8cdf467
externalcharm <freesxsad23@mail.ru>
2022-11-23 16:29:34 +0300

    algorithm added

--- algorithm/Program.cs
+++ algorithm/Program.cs
@@ -1,3 +1,27 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
-// Первоначальный массив можно ввести с калвиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
-// лучше обойтись исключительно массивами.
+﻿void ArrayWithThreeSymbols(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+string[] array = new string[6] { "321", "23", "1234", "hello world", "ser", "2" };
+string[] modifiedArray = new string[array.Length];
+
+ArrayWithThreeSymbols(array, modifiedArray);
+PrintArray(modifiedArray);
